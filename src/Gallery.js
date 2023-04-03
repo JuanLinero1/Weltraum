ba906e866d2a0107c9f2ce2f5063c80575fee81c
juanLinero <juanlinerobonnett611@gmail.com>
2023-04-03 16:42:36 -0500

    changes in images links

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,14 +2,14 @@ function Gallery() {
     const gallery = document.querySelector('.gallery__container');
     //i hate cors
     const locGalleryArr = [  
-        "../public/assets/images/499147.png", 
-        "../public/assets/images/R.png", 
-        "../public/assets/images/OIP.png", 
-        "../public/assets/images/R(3).png",
-        "../public/assets/images/R(2).png", 
-        "../public/assets/images/R(1).png",
-        "../public/assets/images/artemis-1-on-pad-082922.png", 
-        "../public/assets/images/fzwzbv7veaal-kc.png"
+        "https://i.postimg.cc/J0gWYVB2/499147.jpg", 
+        "https://i.postimg.cc/kXznf8Qg/artemis-1-on-pad-082922.jpg", 
+        "https://i.postimg.cc/vZnGjBhL/fzwzbv7veaal-kc.webp", 
+        "https://i.postimg.cc/9Frmd1qk/OIP.jpg",
+        "https://i.postimg.cc/SsSqJGhD/R.jpg", 
+        "https://i.postimg.cc/1X4QK54F/R-1.jpg",
+        "https://i.postimg.cc/MT9wRjZx/R-2.jpg", 
+        "https://i.postimg.cc/8CBNp0q7/R-3.jpg"
         
     ]
 
